d9b656018aa4dc11a6267a9c7a157afa929444e2
Daniel-Oliveira-de-Freitas <danisnake27@gmail.com>
2022-07-05 11:17:28 -0300

    começando o ciBlog

---
+++ application/views/Pages/about.php
@@ -0,0 +1,2 @@
+<h2><?= $title ?></h2>
+<p> this is ciBlog version 1.0 </p>
---
+++ application/views/templates/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>ciBlog</title>
+    <link rel="stylesheet" href="https://bootswatch.com/5/flatly/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/">ciBlog</a>
+        </div>
+        <div id="navbar">
+            <ul class="nav navbar-nav">
+                <li><a href="<?php echo base_url(); ?>">Home</a></li>
+                <li><a href="<?php echo base_url(); ?>/about">About</a></li>
+                
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+   
+    
