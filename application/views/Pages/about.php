6c3531c92eb7db0d11e72faf8356b67e82431b2a
Daniel-Oliveira-de-Freitas <danisnake27@gmail.com>
2022-07-15 21:09:54 -0300

    começando a home e about

--- application/views/Pages/about.php
+++ application/views/Pages/about.php
@@ -1,2 +1,10 @@
-<h2><?= $title ?></h2>
+<h1><?= $title ?></h1>
+<h2>O Que É Um Blog? Uma Introdução ao Blogging</h2>
+<img src="assets\images\O-Que-E-Um-Blog.png">
+<br>
+<p>Um blog é um site ou parte de um site que contém conteúdo frequentemente atualizado sobre um ou múltiplos tópicos. O termo é uma contração para “web log”, que por sua vez significa registrar informações num site.
+
+    O conteúdo num blog vem na forma de artigos em páginas individuais chamados postagens de blog (blog posts). O artigo que você está lendo é um exemplo.
+
+    Na página inicial do blog, você vai encontrar uma coleção dessas postagens apresentadas em ordem cronológica reversa – da mais recente para a mais antiga. A página de Tutoriais da Hostinger é uma boa ilustração para isso.</p>
 <p> this is ciBlog version 1.0 </p>
--- application/views/Pages/home.php
+++ application/views/Pages/home.php
@@ -1,2 +1,60 @@
 <h2><?= $title ?></h2>
 <p> Welcome to the ciBlog application </p>
+
+  <!-- Cards Noticias-->
+  <section class="page-section" id="noticias">
+        <div class="container px-5">
+             <div class="row">
+                <div class="col-md-4 col-sm-12  pb-5">
+                <div class="card">
+                        <img src="assets\images\posts\img.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                        </div>
+                </div>          
+                <div class="col-md-4 col-sm-12 pb-5"> 
+                <div class="card">
+                        <img src="assets\images\posts\images-ex.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                        </div>
+                </div>  
+                <div class="col-md-4 col-sm-12 pb-5"> 
+                <div class="card">
+                            <img src="assets\images\posts\noimage.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                            </div>
+                </div>  
+                
+
+            </div>
+            </div>
+        </div>
+    </section>
+
+   <!-- Fim Cards Noticias-->
+
+   <p>A Anistia Internacional é um movimento global com mais de 10 milhões de pessoas, que realiza ações e campanhas
+    para que os direitos humanos internacionalmente reconhecidos sejam respeitados e protegidos. Estão presentes em
+    mais de 150 países. Todos os dias, alguém, em algum lugar do mundo, recebe apoio da Anistia Internacional. A
+    Avico tem recebido apoio da Anistia Internacional através da Campanha “Omissão não é Política Pública”, e pela
+    qual cobra o Procurador-Geral da República a denunciar integralmente todos os possíveis crimes apontados pela
+    CPI da Covid-19. A Além da Avico, a campanha reúne cerca de vinte entidades da sociedade civil, entre elas
+    estão: Oxfam Brasil, a Associação Brasileira de Saúde Coletiva (Abrasco), o Instituto de Estudos Socioeconômicos
+    (Inesc), o Instituto Brasileiro de Defesa do Consumidor (Idec), Instituto Criola, Conselho Indigenista
+    Missionário (Cimi), Terra de Direitos, Coordenação das Organizações Indígenas da Amazônia Brasileira (Coiab),
+    Movimento Nacional de Direitos Humanos (MNDH), Observatório das Metrópoles, Olodum, Rede Sul-Americana para as
+    Migrações Ambientais (Resama), Justiça Global, Cedeca, Engajamundo, Instituto de Mulheres Negras de Mato Grosso
+    (Imune), Associação Brasileira de Médicas e Médicos pela Democracia (ABBMD), Grupo de Apoio Voluntário.</p>
+<p>Acesse o link e conheça a campanha: <a href=" https://anistia.org.br/campanha/omissao-nao-e-politica-publica/">
+        https://anistia.org.br/campanha/omissao-nao-e-politica-publica/</a>
